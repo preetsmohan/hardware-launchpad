0b1cda50b35e5a45bfc4e3a44a6c37a81bf3388b
pvselvan <pvselvan@umich.edu>
2015-09-19 19:10:51 -0400

    changed registration fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919160145) do
+ActiveRecord::Schema.define(version: 20150919213650) do
 
   create_table "hardwares", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20150919160145) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
