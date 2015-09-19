0d1aeef620c371efc75b82fa7ca1a7a203d205e8
Colin Szechy <cszechy@umich.edu>
2015-09-19 17:05:19 -0400

    Cleaned up gitignore, started work on inventory with dummy data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,11 @@ class MainController < ApplicationController
   end
 
   def inventory
+  	@hardwares = []
+  	# let's try some dummy data
+  	@hardwares << { "name" => "Amazon Echo", "available" => 5, 
+  		"total" => 7, "due" => "Jan 1, 2016"}
+  	@hardwares << { "name" => "Moto 360", "available" => 20, 
+  		"total" => 25, "due" => "Dec 1, 2015"}
   end
 end
